41568e817960d6bfee661ef2986e5fc3ff1c5f8d
Anusheel <asingh246@sapient.com>
2016-05-06 12:42:27 +0530

    Last letter of the word will be capitalised

--- trie.js
+++ trie.js
@@ -8,46 +8,65 @@ class TRIENODE {
     }
 
     insert(child = '') {
-        this.count++;
+        let targetChild,
+            targetLetter;
+
+        child = child.toLowerCase();
         if (child.length > 0) {
-            let targetChild = this.children.get(child[0]);
+            this.count++;
+            targetChild = this.children.get(child[0].toUpperCase());
+            if (targetChild == undefined) {
+                targetChild = this.children.get(child[0]);
+                targetLetter = child[0];
+            }else{
+                targetLetter = child[0].toUpperCase();
+            }
+
             if (targetChild != undefined) {
                 targetChild.insert(child.substring(1));
             } else {
-                this.children.set(child[0], new TRIENODE(child.substring(1)));
+                this.children.set(targetLetter, new TRIENODE(child.substring(1)));
+            }
+
+            if(targetLetter != undefined && child.length === 1 && targetLetter != targetLetter.toUpperCase()){
+                this.children.set(targetLetter.toUpperCase(), this.children.get(targetLetter));
+                this.children.delete(targetLetter);
             }
         }
     }
 
     print(data = '') {
-      console.log(data + '->' + this.children.size + '->' + this.count);
-      if(this.children.size === 0){
-        console.log(data);
-      }else{
-        this.children.forEach((value, key) => {
-     value.print(data + key);
-     })
-      }
-    }
-    
-    search(data){
-      let targetChild = this.children.get(data[0]);
-      if(targetChild != undefined){
-        if(data.length > 1){
-          return targetChild.search(data.substring(1));
-        }else{
-          return true;
+        console.log(data + '->' + this.children.size + '->' + this.count);
+        if (this.children.size === 0) {
+            console.log(data);
+        } else {
+            this.children.forEach((value, key) => {
+                value.print(data + key);
+            })
         }
-      }
-      return false;
     }
 
-    toJSON(){
-      let result = {};
-      this.children.forEach((value, key) => {
-        result[key] = value.toJSON();
-      })
-      return result;
+    search(data) {
+        let targetChild = this.children.get(data[0]);
+
+        targetChild = (targetChild == undefined) ? this.children.get(data[0].toUpperCase()) : targetChild;
+
+        if (targetChild != undefined) {
+            if (data.length > 1) {
+                return targetChild.search(data.substring(1));
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    toJSON() {
+        let result = {};
+        this.children.forEach((value, key) => {
+            result[key] = value.toJSON();
+        })
+        return result;
     }
 }
 
